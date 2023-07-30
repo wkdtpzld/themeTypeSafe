58379c10a8451dc6ebbb7a987d91525a9a33d706
wkdtpzld <slwhswk9@gmail.com>
2023-07-30 18:23:03 +0900

    💄 Style 초기화

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import {theme} from "./utill/theme";
 import {ThemeProvider} from "@emotion/react";
+import "./style/index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
